27d9305abda9e867cbabc191b4a56a09432a96e3
GustavoDCC <gdcordova@icloud.com>
2023-10-13 20:57:35 +0200

    Test driven project example

---
+++ conftest.py
@@ -0,0 +1,9 @@
+import pytest
+from product import Product
+
+@pytest.fixture
+def product():
+    return Product(2, 2)
+
+def __init__():
+    pass
---
+++ test/test_product.py
@@ -0,0 +1,29 @@
+from product import *
+
+
+class TestProduct:
+    def test_initialize_product(self):
+        product = Product(1, 2)
+        assert product.days_remaining == 1
+        assert product.quality == 2
+
+    def test_tick_fresh(self):
+        # Quality for a fresh product is decreased by 1
+        product = Product(2, 3)
+        product.tick()
+        assert product.days_remaining == 1
+        assert product.quality == 2
+
+    def test_tick_past_due(self):
+        # Quality for a product past due is decreased by 2
+        product = Product(0, 3)
+        product.tick()
+        assert product.days_remaining == -1
+        assert product.quality == 1
+
+    def test_tick_rotten(self):
+        # Quality for a rotten product remains 0
+        product = Product(0, 0)
+        product.tick()
+        assert product.days_remaining == -1
+        assert product.quality == 0
